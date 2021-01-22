eca7d09d6f4b8c96e0c74f26328b08f6d9b816c6
Zach Marinov <Marinov.Z@etoncollege.org.uk>
2021-01-22 12:04:53 +0000

    Initial Commit

---
+++ Tamagotchi/TamagotchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiApp.swift
+//  Tamagotchi
+//
+//  Created by Marinov, Zachary (Coll) on 22/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
